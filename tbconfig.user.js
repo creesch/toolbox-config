0c3d051c827fb4987f3a765cbeaca9de4c5ff5a9
Neil Layne <n.layne@gmail.com>
2013-05-27 19:58:29 -0400

    can now edit existing removal reasons and add new ones

--- tbconfig.user.js
+++ tbconfig.user.js
@@ -9,6 +9,7 @@
 // ==/UserScript==
 
 function main() {
+    //console.log('dev script');
 
     var toolbox = $('#moderation_tools').find('.content'),
         configLink = '<li><span class="separator"></span>&nbsp;&nbsp;&nbsp;<a href="javascript:;" class="toolbox-edit">edit toolbox</a></li>',
@@ -18,7 +19,7 @@ function main() {
     var config = {
         domainColors: '',
         removalReasons: '',
-        mailMacros: ''
+        mailMacros: '',
     }
 
     if (!subreddit) return;
@@ -73,14 +74,15 @@ function main() {
 <div class="tb-settings tb-form">\
 <p><a class="reason-settings" href="javascript:;">removal reason settings</a> &nbsp; &nbsp; <a class="edit-reasons" href="javascript:;">removal reasons</a> \
 &nbsp; &nbsp; <a class="edit-domains" href="javascript:;">domain colors</a> &nbsp; &nbsp; <a class="tb-local-settings" href="javascript:;">toolbox settings</a> \
+&nbsp; &nbsp; <a href="http://www.reddit.com/r/' + subreddit + '/wiki/toolbox">toolbox wiki page</a>\
+&nbsp; &nbsp; <a href="http://www.reddit.com/r/' + subreddit + '/wiki/automoderator">automoderator wiki page</a>\
 &nbsp; &nbsp; <a class="tb-close" href="javascript:;">close</a></p>\
 <div class="reasosn-notice" style="display:none;">\
 <br><br><p>Removal reasons were found in your CSS but have not been saved to the wiki configuration page.<br />\
 You will need to save them to the wiki before you can edit them. &nbsp;Would you like to do so now?<br />\
-<a class="update-reasons" href="javascript:;">Save removal reasons to wiki</a> (note: this requires that you have the wiki enabled and wiki permisissions)</p>\
+<a class="update-reasons" href="javascript:;">Save removal reasons to wiki</a> (note: this requires that you have wiki editing permisissions)</p>\
 </div></div>\
-        ';
-        
+    	';
         $(html).appendTo('body').show();
     }
 
@@ -124,7 +126,7 @@ You will need to save them to the wiki before you can edit them. &nbsp;Would you
                     bantitle: XML.find('bantitle').text() || '',
                     getfrom: XML.find('getfrom').text() || '',
                     reasons: reasons
-                };
+                }
 
                 $('.reasosn-notice').show();
 
@@ -141,31 +143,88 @@ You will need to save them to the wiki before you can edit them. &nbsp;Would you
             $('.tb-settings').remove();
         });
 
+    $('body').delegate('.edit-reasons', 'click', function () {
+
+            var html = $('\
+<div class="edit-reasons-form tb-form">\
+<p><textarea class="edit-area" style="width: 800px; height: 150px;"></textarea></p>\
+<p><input class="save" type="button" value="new"/>&nbsp;&nbsp;&nbsp;<input class="cancel" type="button" value="cancel" /></p>\
+<table><tbody /></table></div>\
+');
+            $(html).appendTo('body').show();
+
+            var i = 0;
+            $(config.removalReasons.reasons).each(function () {
+                    $(html).find('tbody').append('<tr><th><input type="radio" style="display:none;" reason="' + i + '"name="reason-' + subreddit + '" id="reason-' + subreddit + '-' + i + '"></th><td><label for="reason-' + subreddit + '-' + (i++) + '">' + unescape(this) + '<BR></label></td></tr>');
+                });
+
+            $('th input[type=radio]').change(function () {
+                    $(html).find('.save').val('save');
+                    var reasonsNum = $(this).attr('reason');
+                    $('.edit-area').val(unescape(config.removalReasons.reasons[reasonsNum]));
+                    $('.edit-area').attr('reason', reasonsNum);
+                });
+
+            // Do things about stuff.
+            $(html).delegate('.save', 'click', function () {
+                    var reasonsNum = $('.edit-area').attr('reason');
+                    var reasonText = $('.edit-area').val();
+                    if (reasonsNum) {
+                        config.removalReasons.reasons[reasonsNum] = escape(reasonText);
+                    } else {
+                        config.removalReasons.reasons.push(escape(reasonText));
+                    }
+                    postToWiki();
+                    $(html).remove();
+                });
+
+            $(html).delegate('.cancel', 'click', function () {
+                    $(html).remove();
+                });
+
+        });
+
     $('body').delegate('.reason-settings', 'click', function () {
+            console.log(config.removalReasons);
             var html = '\
-            <div class="edit-reasosn tb-form">\
-                <p>getfrom:&nbsp;<input class="getfrom" type="text" value="' + config.removalReasons.getfrom + '"/>&nbsp; &nbsp;\
-                logsub:&nbsp;<input name="logsub" type="text" "' + config.removalReasons.logsub + '"/>&nbsp; &nbsp;&nbsp;</p>\
-                <p>pmsubject:&nbsp;<input class="pmsubject" style="width: 600px;" type="text" value="' + config.removalReasons.pmsubject + '"/></p>\
-                <p>logtitle:&nbsp;<input class="logtitle" style="width: 600px;" type="text" value="' + config.removalReasons.logtitle + '"/></p>\
-                <p>bantitle:&nbsp;<input class="bantitle" style="width: 600px;" type="text" value="' + config.removalReasons.bantitle + '"/></p>\
-                <p>logreason:&nbsp;<input class="logreason" style="width: 600px;" type="text" value="' + config.removalReasons.logreason + '"/></p>\
+            <div class="reason-setting-form tb-form">\
+               <p>getfrom /r/:&nbsp;<input class="getfrom" type="text" value="' + (config.removalReasons.getfrom || '') + '"/> (note: this setting overrides all other settings.)  &nbsp;\
+                logsub /r/:&nbsp;<input class="logsub" type="text" value="' + (config.removalReasons.logsub || '') + '"/>&nbsp; &nbsp;&nbsp;</p>\
+                <p>pmsubject:&nbsp;<input class="pmsubject" style="width: 600px;" type="text" value="' + (config.removalReasons.pmsubject || '') + '"/></p>\
+                <p>logtitle:&nbsp;<input class="logtitle" style="width: 600px;" type="text" value="' + (config.removalReasons.logtitle || '') + '"/></p>\
+                <p>bantitle:&nbsp;<input class="bantitle" style="width: 600px;" type="text" value="' + (config.removalReasons.bantitle || '') + '"/></p>\
+                <p>logreason:&nbsp;<input class="logreason" style="width: 600px;" type="text" value="' + (config.removalReasons.logreason || '') + '"/></p>\
                 <span>Header:</span>\
-                <p><textarea class="edit-header" style="width: 600px; height: 100px;">' + unescape(config.removalReasons.header) + '</textarea></p>\
+                <p><textarea class="edit-header" style="width: 600px; height: 100px;">' + unescape(config.removalReasons.header || '') + '</textarea></p>\
                 <span>Footer:</span>\
-                <p><textarea class="edit-footer" style="width: 600px; height: 100px;">' + unescape(config.removalReasons.footer) + '</textarea></p>\
+                <p><textarea class="edit-footer" style="width: 600px; height: 100px;">' + unescape(config.removalReasons.footer || '') + '</textarea></p>\
                 <p><input class="save" type="button" value="save" />&nbsp;&nbsp;&nbsp;<input class="cancel" type="button" value="cancel" /></p>\
             </div>\
         ';
             $(html).appendTo('body').show();
 
             // Do things about stuff.
-            $('.edit-reasosn').delegate('.save', 'click', function () {
+            $('.reason-setting-form').delegate('.save', 'click', function () {
 
+                    config.removalReasons = {
+                        pmsubject: $('.pmsubject').val(),
+                        logreason: $('.logreason').val(),
+                        header: escape($('.edit-header').val()),
+                        footer: escape($('.edit-footer').val()),
+                        logsub: $('.logsub').val(),
+                        logtitle: $('.logtitle').val(),
+                        bantitle: $('.bantitle').val(),
+                        getfrom: $('.getfrom').val()
+                    };
+                    console.log(config.removalReasons);
+
+                    postToWiki();
+
+                    $('.reason-setting-form').remove();
                 });
 
-            $('.edit-reasosn').delegate('.cancel', 'click', function () {
-                    $('.edit-reasosn').remove();
+            $('.reason-setting-form').delegate('.cancel', 'click', function () {
+                    $('.reason-setting-form').remove();
                 });
         });
 }
@@ -175,7 +234,9 @@ You will need to save them to the wiki before you can edit them. &nbsp;Would you
 
         // CSS
         var css = '\
-  	.tb-form {background-color: white; z-index:10000; position:fixed; top:0px; left:0px; right:0px; bottom:0px;}\
+        .tb-form {background-color: white; z-index:10000; position:fixed; top:0px; left:0px; right:0px; bottom:0px; overflow:auto;}\
+        .edit-reasons-form tbody tr{ vertical-align:top; border:1px solid rgb(187, 187, 187);display:block;margin-bottom: 5px;font-size: 115%;font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;padding:5px }\
+        .edit-reasons-form th{ padding-right:10px }\
         ';
 
         // Add CSS.
